5672363499aa6d1e606e11b4bf9756cd13d52b84
yilber.calderon <yilberandrey@gmail.com>
2017-03-23 10:43:32 -0500

    seagrega comentario con el batch abierto

--- PruebaGitCon/Program.cs
+++ PruebaGitCon/Program.cs
@@ -14,6 +14,8 @@ namespace PruebaGitCon
             Console.WriteLine("bueno q paso aca que no cojio el primer commit???");
             //bueno en git hub si guardan los commits, ademas , si uno da clik derecho una clase conc ambios , y da clik
             //en undo, luego no puede recuperar lo que habia cambiado
+
+            Console.WriteLine("Ahora , este comentario tiene el bash abierto, quedara en el historiald evusal ?");
         }
     }
 }
